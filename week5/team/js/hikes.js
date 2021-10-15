a80fe6de92c293589b46bff49711b21bfc0a9de1
en3m3 <71306799+en3m3@users.noreply.github.com>
2021-10-14 20:44:24 -0600

    default

--- week5/team/js/hikes.js
+++ week5/team/js/hikes.js
@@ -1,5 +1,5 @@
 //create an array of hikes
-export const hikeList = [
+const hikeList = [
     {
       name: "Bechler Falls",
       imgSrc: "falls.jpg",
@@ -33,3 +33,5 @@ export const hikeList = [
         "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
     }
   ];
+
+  export default {hikeList};
