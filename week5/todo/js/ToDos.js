2ad01ea1e094729300dace4d0f1fa3323bce9b27
en3m3 <71306799+en3m3@users.noreply.github.com>
2021-10-16 15:13:23 -0600

    test

--- week5/todo/js/ToDos.js
+++ week5/todo/js/ToDos.js
@@ -1,35 +1,18 @@
-(function(){
-    "use strict";
-
-var Todo = function() {
+export var Todo = function() {
     this.version = '1.0.0';
     this.errors = [];
 }
 
-    Todo.prototype = function()
-    {
-        constructor: Todo,
-        taskList = [],
-        container = document.getElementById(container),
-        colorScheme = props['colorScheme'],
-        height = props['height'] || '500px',
-        width = props['width'] || '400px',    
-    
-        addTask = function(){
+Todo.prototype = function()
+{
+    constructor: Todo,
+    taskList = [],
+    container = document.getElementById(container),
+    colorScheme = props['colorScheme'],
+    height = props['height'] || '500px',
+    width = props['width'] || '400px',    
 
-        }
-    };
+    addTask = function(){
 
-
-
-    // class Todo {
-    //    constructor(container, props) {
-        //    var taskList = new Array();
-        //    console.log(props);
-        //    this.container = document.getElementById(container);
-        //    this.colorScheme = props['colorScheme'] || 'light';
-        //    this.height = props['height'] || '500px';
-        //    this.width = props['width'] || '400px';
-    //    }
-// 
-});
+    }
+};
